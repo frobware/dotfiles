30c908235651cda98de56857226e3a3661e91aa8
Andrew McDermott <andrew.iain.mcdermott@gmail.com>
2014-01-18 19:49:12 +0000

    Add cmd-key-happy preferences

--- install-links.sh
+++ install-links.sh
@@ -71,5 +71,6 @@ done
 
 install_link "$PWD/Resources/Xresources" "$HOME/.Xresources"
 install_link "$PWD/tmux.conf" "$HOME/.tmux.conf"
+install_link "$PWD/cmd-key-happy.lua" "$HOME/.cmd-key-happy.lua"
 
 exit 0
