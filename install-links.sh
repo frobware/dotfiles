ffb801584448bd2bc493d02b1d42c1b718d9734c
Andrew McDermott <aim@frobware.com>
2014-12-19 08:53:21 +0000

    Add cmd-key-happy.rc example

--- install-links.sh
+++ install-links.sh
@@ -62,6 +62,7 @@ done
 install_link "$PWD/Resources/Xresources" "$HOME/.Xresources"
 install_link "$PWD/tmux.conf" "$HOME/.tmux.conf"
 install_link "$PWD/cmd-key-happy.lua" "$HOME/.cmd-key-happy.lua"
+install_link "$PWD/cmd-key-happy.rc" "$HOME/.cmd-key-happy.rc"
 install_link "$PWD/Resources/gnomerc" "$HOME/.gnomerc"
 
 exit 0
