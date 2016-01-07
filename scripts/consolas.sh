730adf45c9bcf15cbdf984a7a533c916b6cb29bd
Andrew McDermott <aim@frobware.com>
2016-01-07 23:23:02 +0000

    Ensure cabextract gets installed

--- scripts/consolas.sh
+++ scripts/consolas.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 set -x
+type -p cabextract || sudo apt-get install cabextract
 mkdir temp
 cd temp
 wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
