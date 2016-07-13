81a0225c95271ee4324fb2742864e330af43e6c5
Andrew McDermott <aim@frobware.com>
2016-07-13 21:55:49 +0100

    Adapt to new location for Juju 2.0's bash completion

--- Bash/features/juju-dev.bash
+++ Bash/features/juju-dev.bash
@@ -1,7 +1,7 @@
 export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
 
-[ -f ~/juju/etc/bash_completion.d/juju2 ] && \
-    .  ~/juju/etc/bash_completion.d/juju2
+[ -f ~/juju/etc/bash_completion.d/juju-2.0 ] && \
+    .  ~/juju/etc/bash_completion.d/juju-2.0
 
 provide juju-dev
