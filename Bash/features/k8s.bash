4fe5c1c5db5ed334c1db354ebaa693390f5471ae
Andrew McDermott <aim@frobware.com>
2017-09-05 15:59:07 +0100

    bash/features/k8s: tweaks

--- Bash/features/k8s.bash
+++ Bash/features/k8s.bash
@@ -4,12 +4,12 @@ function x {
 }
 
 function localkube {
+    export KUBERNETES_PROVIDER=local
     export PATH=$HOME/go/src/k8s.io/kubernetes/cluster:$PATH
     kubectl.sh config set-cluster local --server=https://localhost:6443 --certificate-authority=/var/run/kubernetes/server-ca.crt
     kubectl.sh config set-credentials myself --client-key=/var/run/kubernetes/client-admin.key --client-certificate=/var/run/kubernetes/client-admin.crt
     kubectl.sh config set-context local --cluster=local --user=myself
     kubectl.sh config use-context local
-
 }
 
 function localkubeproxy {
