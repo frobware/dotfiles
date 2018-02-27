5e096247354480a94b8c486b6ad222f1d24555b4
Andrew McDermott <aim@frobware.com>
2018-02-27 22:07:09 +0000

    Delete lxc-dev feature

--- bash_features/lxc-dev.bash
+++
@@ -1,7 +0,0 @@
-export GOPATH=~/go-projects/lxd
-export PATH=$GOPATH/bin:$PATH
-
-[ -f $HOME/go-projects/lxd/src/github.com/lxc/lxd/config/bash ] && \
- source $HOME/go-projects/lxd/src/github.com/lxc/lxd/config/bash ]
-
-provide lxc-dev
