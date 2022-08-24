3b22ad78d7052d07832ff20f9c3569df434d45a8
Andrew McDermott <aim@frobware.com>
2022-08-24 09:38:23 +0100

    darwin/automounts: replace makefile with install script

---
+++ automounts/darwin/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+sudo install auto_master /etc/auto_master
+
+# The following /etc/auto_<X> files must be 0600 when installed or
+# they will silently fail.
+
+sudo install -m 644 auto_u /etc/auto_u
+sudo install -m 644 auto_r /etc/auto_r
+
+sudo automount -vc
